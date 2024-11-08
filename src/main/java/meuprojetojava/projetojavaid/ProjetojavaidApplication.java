88d7256a411dcc9fe6485ed2ad14a26bcc56f602
YuriRamalhoSilva <yuriramalhosilva67@gmail.com>
2024-11-07 22:14:07 -0300

    Criação do projeto JAVA

---
+++ src/main/java/meuprojetojava/projetojavaid/ProjetojavaidApplication.java
@@ -0,0 +1,13 @@
+package meuprojetojava.projetojavaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojavaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojavaidApplication.class, args);
+	}
+
+}
